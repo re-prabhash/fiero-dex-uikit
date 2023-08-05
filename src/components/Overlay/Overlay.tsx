9c93eff5ab8be1e963ec83e960217c799b635d63
re-shiwani <re-shiwani@mobiloitte.com>
2023-08-05 18:25:05 +0530

    fix modal add backdrop filter and background

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -2,15 +2,30 @@ import styled from "styled-components";
 import { OverlayProps } from "./types";
 
 const Overlay = styled.div.attrs({ role: "presentation" })<OverlayProps>`
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100%;
-  background-color: #000000b3;
+
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    z-index: -1;
+    position: fixed;
+    align-items: center;
+    backdrop-filter: blur(4px);
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.5);
+    -webkit-tap-highlight-color: transparent;
   transition: opacity 0.4s;
-  opacity: ${({ show }) => (show ? 0.6 : 0)};
-  z-index: ${({ zIndex }) => zIndex};
+
+  // position: fixed;
+  // top: 0px;
+  // left: 0px;
+  // width: 100%;
+  // height: 100%;
+  // background-color: #000000b3;
+  // transition: opacity 0.4s;
+  // opacity: ${({ show }) => (show ? 0.6 : 0)};
+  // z-index: ${({ zIndex }) => zIndex};
   pointer-events: ${({ show }) => (show ? "initial" : "none")};
 `;
 
