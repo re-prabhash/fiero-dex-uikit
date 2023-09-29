34d1df0b23e9744f8ec79a029f452bb1f4378bc6
re-suraj <re-suraj@mobiloitte.com>
2023-09-29 10:35:42 +0530

    change logo on top

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <figure viewBox="0 0 32 32" {...props}>
-      <img width="32" height="32" src="images/footer_logo.png" alt="" />
+      <img width="32" height="32" src="images/LogoTextNewDark.png" alt="" />
     </figure>
   );
 };
